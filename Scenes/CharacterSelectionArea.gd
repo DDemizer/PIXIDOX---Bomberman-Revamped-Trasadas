4f329615b05afa3792d323d28b4f6b2fe4fc9434
Rops-11 <jrofer.casio11@gmail.com>
2024-07-18 23:13:28 +0800

    Adding CharacterSelectionManager and CharacterSelection Scripts Adjustments

--- Scenes/CharacterSelectionArea.gd
+++ Scenes/CharacterSelectionArea.gd
@@ -15,6 +15,21 @@ extends Control
 @export var player3Activated = false
 @export var player4Activated = false
 
+# Buttons
+var general_start_button = Input.is_action_just_pressed("general_start_button")
+var up = 11
+var down = 12
+var left = 13
+var right = 14
+var start_button = 6
+var select_button = 4
+var green = 2
+var blue = 3
+var yellow = 0
+var red = 1
+var l1 = 9
+var r1 = 10
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	cursor1.process_mode = 4
@@ -30,43 +45,46 @@ func _process(delta):
 		print_debug('Player 2 Active: ', player2Activated)
 		print_debug('Player 3 Active: ', player3Activated)
 		print_debug('Player 4 Active: ', player4Activated)
+		print_debug('Player 1 Character: ', CharacterSelectionManager.player1)
+		print_debug('Player 2 Character: ', CharacterSelectionManager.player2)
+		print_debug('Player 3 Character: ', CharacterSelectionManager.player3)
+		print_debug('Player 4 Character: ', CharacterSelectionManager.player4)
 		
-#		print_debug('Controller 1 : Player ', ctrl1PlayerNumber if (ctrl1PlayerNumber == null) else ctrl1PlayerNumber + 1)
-#		print_debug('Controller 2 : Player ', ctrl2PlayerNumber if (ctrl2PlayerNumber == null) else ctrl2PlayerNumber + 1)
-#		print_debug('Controller 3 : Player ', ctrl3PlayerNumber if (ctrl3PlayerNumber == null) else ctrl3PlayerNumber + 1)
-#		print_debug('Controller 4 : Player ', ctrl4PlayerNumber if (ctrl4PlayerNumber == null) else ctrl4PlayerNumber + 1)
-#		print_debug(cursor1.process_mode)
-	
-	var general_start_button = Input.is_action_just_pressed("general_start_button")
-	var up = 11
-	var down = 12
-	var left = 13
-	var right = 14
-	var start_button = 6
-	var select_button = 4
 	
 	if player1Activated == false:
 		if Input.is_joy_button_pressed(0, start_button):
 			player1Activated = true
-				cursor1.process_mode = 0
+			cursor1.process_mode = 0
 			cursor1.texture = player1_texture
+			CharacterSelectionManager.players_in_game += 1
 			print_debug('Player 1 Activated')
 	if player2Activated == false:
 		if Input.is_joy_button_pressed(1, start_button):
 			player2Activated = true
 			cursor2.process_mode = 0
 			cursor2.texture = player2_texture
+			CharacterSelectionManager.players_in_game += 1
 			print_debug('Player 2 Activated')
 	if player3Activated == false:
 		if Input.is_joy_button_pressed(2, start_button):
 			player3Activated = true
 			cursor3.process_mode = 0
 			cursor3.texture = player3_texture
+			CharacterSelectionManager.players_in_game += 1
 			print_debug('Player 3 Activated')
 	if player4Activated == false:
 		if Input.is_joy_button_pressed(3, start_button):
 			player4Activated = true
 			cursor4.process_mode = 0
 			cursor4.texture = player4_texture
+			CharacterSelectionManager.players_in_game += 1
 			print_debug('Player 4 Activated')
-
+			
+	if CharacterSelectionManager.players_ready == CharacterSelectionManager.players_in_game:
+		$ReadyLabel.show()
+		if Input.is_action_just_pressed("general_start_button"):
+			get_tree().change_scene_to_file("res://world.tscn")
+	else:
+		$ReadyLabel.hide()
+		
+	
---
+++ Scenes/CharacterSelectionManager.gd
@@ -0,0 +1,26 @@
+extends Node
+
+var players_in_game = 0
+var players_ready = 0
+
+var player1
+var player2
+var player3
+var player4
+
+var characters = {
+	'raccoon' : preload("res://character-scenes/raccoon.tscn"),
+	'cat' : preload("res://character-scenes/raccoon.tscn"),
+	'beaver' : preload("res://character-scenes/raccoon.tscn"),
+	'monkey' : preload("res://character-scenes/raccoon.tscn")
+}
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
