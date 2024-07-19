f788212dd89dc0d05815586fccd56b8f40db91d4
Manawkie <samenianjoshua@gmail.com>
2024-07-19 18:53:20 +0800

    build mechanic finish

--- maps/world_desert.gd
+++ maps/world_desert.gd
@@ -2,33 +2,42 @@ extends Node2D
 
 
 @onready var tile_map = $Map
-#@onready var character = $Raccoon.global_position
-#@onready var charToTileMapPosition = tile_map.to_local(character)
-#@onready var charCellcoords = tile_map.world_to_map(charToTileMapPosition)
-#@onready var charExactTile = tile_map.get_cellv(character)
+@onready var tile_map2 = $Desert_things
+var char_pos
+var character_Position : Vector2i
+var character
+var charToTileMapPosition : Vector2i
+var atlastCoords : Vector2i
+var tile_data : TileData
+var source_id_WOphysics = 2
+var source_id_Wphysics = 1
+
 
 var sand = 1
+var placed_by_player_custom_data = "placed_by_player"
 
 		
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 	
-	
+func _process(delta):
+	char_pos = $Raccoon.global_position
+	character_Position = tile_map.local_to_map(char_pos)
+	if character_Position != charToTileMapPosition:
+		tile_map.set_cell(sand, charToTileMapPosition, source_id_Wphysics, atlastCoords)
 
 
 func _input(event):
+	print_debug(event)
 	if Input.is_action_just_pressed("place_block"):
-		print("ze bomb has been planted")
-		var character = $Raccoon.global_position
-		var charToTileMapPosition : Vector2i = tile_map.local_to_map(character)
-		var source_id = 1
-		var atlastCoords: Vector2i = Vector2i(1,0)
+		#position of the character
+		character = $Raccoon.global_position
+		#position cords
+		charToTileMapPosition = tile_map.local_to_map(character)
+		#id of the picture
 		
-		
-		
-		tile_map.set_cell(sand, charToTileMapPosition, source_id, atlastCoords)
-		
-		
-		
-	
+		#position of the certain tile
+		atlastCoords = Vector2i(1,0)
+		#place a block
+		tile_map.set_cell(sand, charToTileMapPosition, source_id_WOphysics, atlastCoords)
